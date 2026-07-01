f51f88f55671caeb5a711f643ad09242adc9e26a
Sanjay Marki <sanjaymarki@gmail.com>
2026-07-01 16:09:46 +0530

    Bulk userrrwuldfdsfssssaaaaadflkfasassdwa9lai

--- src/app/payment/[userid]/page.tsx
+++ src/app/payment/[userid]/page.tsx
@@ -81,8 +81,8 @@ const PaymentPage = () => {
       const amount = userData.total_amount;
 
       const res = await axios.post("/api/user/createRzpOrder", {
-        //amount: amount * 100,
-        amount: 1000,
+        amount: amount * 100,
+        // amount: 1000,
         currency: "INR",
         receipt_id: `${userData.reg_no}_star`,
         userId: userData._id,
